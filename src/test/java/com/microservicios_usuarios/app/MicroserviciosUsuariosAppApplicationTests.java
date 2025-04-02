7c4dd17683235540381773ce79bb452e330d0ecc
Andres Felipe Perdomo Andrade <felipeanfepe@gmail.com>
2025-04-02 16:02:52 -0500

    eureka

---
+++ src/test/java/com/microservicios_usuarios/app/MicroserviciosUsuariosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios_usuarios.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosUsuariosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
